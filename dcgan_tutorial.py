77f8db09ae4b263d8e48ff4aaa2aa86501619b2b
Tim Karwasz <tk81fevo@studserv.uni-leipzig.de>
2020-06-03 17:18:25 +0200

    Added useful comments and requirements.txt

--- dcgan_tutorial.py
+++ dcgan_tutorial.py
@@ -2,58 +2,65 @@ import torch
 import torchvision
 import torch.nn as nn # Neural Networks functions
 import torch.optim as optim # Optimization algos
-import torchvision.datasets as datasets
+import torchvision.datasets as datasets #das Dataset
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader  # Dataset mangement and for mini batches
-from torch.utils.tensorboard import SummaryWriter # for nice visu
-from models import Generator, Discriminator
+from torch.utils.tensorboard import SummaryWriter # für die Visualization
+from models import Generator, Discriminator # die definierten Modelle
 
 
 
 #Hyperparameters
 
-# learning rate, defined in paper
+# learning rate, defined in paper , https://towardsdatascience.com/understanding-learning-rates-and-how-it-improves-performance-in-deep-learning-d0d4059c1c10 for what it is
 lr = 0.0002
 
 #work batches
 batch_size = 64
 
-#image size important for Conv2dLayers
+#image size important for Conv2dLayers, die Bilder haben zwar 28x28 werden aber duch my_transforms resized
 image_size = 64
 
+# hier 3 für RGB, in diesem Falle 1 da in MNIST nur Grau Bilder sind
 channels_img = 1
 
-#length of noise vector
+#length of noise vector, kann varieren
 channels_noise = 256
 
+#Anzahl der Durchläufe
 num_epochs = 10
 
+#Anzahl der Features die aufgenommen werden, je mehr desto besser, dauert dann aber auch länger
 g_features = 16
 d_features = 16
 
+#Compose ist zum Aneinaderreihen von Bild Veränderungen
 my_transforms = transforms.Compose([
     transforms.Resize(image_size),
+    #konvertiert Bild mit Koordinaten in einen FloatTensor ( siehe https://pytorch.org/docs/stable/torchvision/transforms.html#transforms-on-torch-tensor)
     transforms.ToTensor(),
+    # Normailzes the tensor but does not mutate it
     transforms.Normalize((0.5,), (0.5,)),
     ])
 
-
+#Dataset wird geladen
 dataset = datasets.MNIST(root='dataset/', train = True, transform = my_transforms, download = True)
 dataloader = DataLoader(dataset,batch_size = batch_size, shuffle = True)
 
-
+#Wenn GPU vorhanden die Cuda aktiviert hat, wird diese als Gerät benutzt sonst die CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Create Discriminator and Generator
-
 netD = Discriminator(channels_img, d_features).to(device)
 netG = Generator(channels_noise, channels_img, g_features).to(device)
 
 
 #Setup Optimizer for G and D
+# https://www.youtube.com/watch?v=mdKjMPmcWjY und https://towardsdatascience.com/adam-latest-trends-in-deep-learning-optimization-6be9a291375c als Ressourcen für Adamne
 optimizerD = optim.Adam(netD.parameters(), lr = lr, betas=(0.5, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr = lr, betas=(0.5, 0.999))
 
+#send them into train mode
 netD.train()
 netG.train()
 
@@ -68,14 +75,15 @@ criterion = nn.BCELoss()
 real_label = 1
 fake_label = 0
 
-#to see progession in image generation
+#um forschritt der bilder zu sehen setzen wir die noise fixed und nehmen nicht immer eine andere
 fixed_noise  = torch.randn(64, channels_noise, 1, 1,).to(device)
 
+#erstellt die dirs für tensorboard das dann mit "tensorboard --logdir runs" aufgerufen werden kann
 writer_real = SummaryWriter(f'runs/DCGAN_MNIST/test_real')
 writer_fake = SummaryWriter(f'runs/DCGAN_MNIST/test_fake')
 print("Starting training....")
 
-
+#hier bin ich auch noch nicht komplett durchgestiegen
 for epoch in range(num_epochs):
     for batch_idx, (data, targets) in enumerate(dataloader):
         data = data.to(device)
@@ -88,9 +96,12 @@ for epoch in range(num_epochs):
         netD.zero_grad()
         # all real images
         label = (torch.ones(batch_size) *0.9).to(device)
+
         # between 0 and 1, we want them to be 1
         output = netD(data).reshape(-1)
+
         lossD_real = criterion(output, label)
+
         # compute mean confidence
         D_x = output.mean().item()
 
@@ -115,7 +126,7 @@ for epoch in range(num_epochs):
         lossG.backward()
         optimizerG.step()
 
-
+        #das ist einfach nur Print um zu sehen wie weit man ist und um Bilder für tensorboard zu generieren
         if batch_idx % 100 == 0:
             print(f'Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(dataloader)} \
                 Loss D: {lossD:.4f}, Loss G: {lossG:.4f} D(x): {D_x:.4f} ')
--- models.py
+++ models.py
@@ -7,13 +7,15 @@ class Discriminator(nn.Module):
     def __init__(self, img_channels, d_features):
         super(Discriminator, self).__init__()
         self.net = nn.Sequential(
-            # img_channels(3) x 64p x 64p
+            # Anfangsgröße, img_channels(3 weil RGB, bei grau nur 1) x 64p x 64p
             nn.Conv2d( img_channels, d_features, kernel_size = 4, stride = 2, padding = 1),
             nn.LeakyReLU(0.2),
+            #nach dem ersten filter erhalten wir diese größe : ( https://youtu.be/sTTuaNp5LTw?list=PL1GQaVhO4f_jLxOokW7CS5kY_J1t1T17S&t=107) 
             # d_features x 32p x 32p
             nn.Conv2d( d_features, d_features*2, kernel_size = 4, stride = 2, padding = 1),
             nn.BatchNorm2d(d_features*2),
             nn.LeakyReLU(0.2),
+            # dann diese größe und immer so weiter
             # d_features*2 x 16p x 16p
             nn.Conv2d( d_features * 2, d_features*4, kernel_size = 4, stride = 2, padding = 1),
             nn.BatchNorm2d(d_features*4),
@@ -24,7 +26,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2),
             # d_features*8 x 4p x 4p
             nn.Conv2d( d_features * 8, 1, kernel_size = 4, stride = 2, padding = 0),
-            # ----> 1 x 1 x 1
+            # ----> 1 x 1 x 1, am Ende wird entschieden ob das Bild das verarbeitet wurde, echt war oder nicht
             nn.Sigmoid()
             )
 
@@ -36,6 +38,7 @@ class Generator(nn.Module):
     def __init__(self, noise_channels , img_channels, g_features):
         super(Generator, self).__init__()
         self.net = nn.Sequential(
+            #genau das gleiche wie Discriminator nur umgekehrt
             # noise_channels x 1 x 1
             nn.ConvTranspose2d(noise_channels, g_features*16, kernel_size = 4, stride = 1, padding = 0),
             nn.BatchNorm2d(g_features*16),
